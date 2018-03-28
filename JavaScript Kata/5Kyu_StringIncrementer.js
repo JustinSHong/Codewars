f5a3c24ce6a4dc4253092fdf96382ef72de12363
Justin Hong <jhong2009@gmail.com>
2018-03-27 21:33:06 -0700

    set up incrementString fx to increment any nums in strng

--- JavaScript Kata/5Kyu_StringIncrementer.js
+++ JavaScript Kata/5Kyu_StringIncrementer.js
@@ -2,9 +2,40 @@
 // If the string already ends with a number, the number should be incremented by 1. 
 // If the string does not end with a number the number 1 should be appended to the new string.
 
+function incrementString (strng) {
+  // use regex to find numbers in str, if any
+  let regex1 = /\d/;
+  let regex2 = /[1-9]/;
+  // loop through str and check for nums
+  let chars = strng.split('').filter((char) => {
+    return regex1.test(char);
+  });
+  // store non-number chars in result
+  let result = strng.slice(0, strng.indexOf(chars[0]));
+  let num;
+  // check if any nums were found, if not add 1 to strng
+  if (chars.length === 0) {
+    return strng + `${1}`;
+  } 
+  // nums were found, iterate the num in strng
+  else {
+    // convert digits in char to a whole num
+    num = parseInt(chars.join(''), 10);
 
-
-
+    // add leading 0s to result, if any
+    // a leading 0 occurs before any non-zero digit
+    if (strng.search(regex2) > strng.indexOf(0)) {
+    	for (let i = 0; i < chars.length - num.length; i++) {
+    		result += "0";
+    	}
+    }
+    // increment num by 1
+    num++;
+  }
+  // output: original string with numbers added to the end
+  result += num.toString();
+  return result;
+}
 
 // test cases
 incrementString("foobar000"); // "foobar001")
