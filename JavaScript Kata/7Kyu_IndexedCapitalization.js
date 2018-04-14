2cdb54ec9a15417a2ba0b30bc2f2fcceb7577831
Justin Hong <jhong2009@gmail.com>
2018-04-14 16:27:15 -0700

    set up problem description and test cases

---
+++ JavaScript Kata/7Kyu_IndexedCapitalization.js
@@ -0,0 +1,16 @@
+// Description:
+// Given a string and an array of integers representing indices,
+// capitalize all letters at the given indices.
+
+// For example:
+//     capitalize("abcdef",[1,2,5]) = "aBCdeF"
+//     capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+// The input will be a lowercase string with no spaces and an array of digits.
+
+// test cases
+capitalize("abcdef", [1, 2, 5]); // 'aBCdeF'
+capitalize("abcdef", [1, 2, 5, 100]); // 'aBCdeF'
+capitalize("codewars", [1, 3, 5, 50]); // 'cOdEwArs'
+capitalize("abracadabra", [2, 6, 9, 10]); // 'abRacaDabRA'
+capitalize("codewarriors", [5]); // 'codewArriors'
+capitalize("indexinglessons", [0]); // 'Indexinglessons'
