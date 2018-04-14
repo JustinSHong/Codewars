6f650f610e85714911d1d430c6cedf4391657e49
Justin Hong <jhong2009@gmail.com>
2018-04-14 15:30:04 -0700

    upload solution using for loop

--- JavaScript Kata/6Kyu_VowelConsonantLexicon.js
+++ JavaScript Kata/6Kyu_VowelConsonantLexicon.js
@@ -18,6 +18,63 @@
 
 // Vowels will be any of "aeiou". Input will be a lowercase string, no spaces.
 
+// Lexicographically minimal string rotation - problem of finding the rotation of a string
+// with the lowest lexicographical order of all possible rotations
+// Lexicographical Order - ) is a generalization of the way words are
+// alphabetically ordered based on the alphabetical order of their component letters
+
+// input: lowercase string with vowels and consonants
+// define a set of vowels
+// separate consonants and vowels and store them in arrays
+// order the consonants and vowels from lowest unicode value to the highest
+// alternate vowels and consonants from lowest unicode to the highest
+// output: string with the lowest lexicographically string rotation
+
+function solve(s) {
+	let result = "";
+	let regex = /[aeiou]/;
+	let consonants = s
+		.split("")
+		.filter(char => {
+			return !regex.test(char);
+		})
+		.sort((a, b) => {
+			return a.charCodeAt(0) - b.charCodeAt(0);
+		});
+	let vowels = s
+		.split("")
+		.filter(char => {
+			return regex.test(char);
+		})
+		.sort((a, b) => {
+			return a.charCodeAt(0) - b.charCodeAt(0);
+		});
+	if (consonants.length > vowels.length) {
+		for (let i = 0; i < consonants.length; i++) {
+			if (!vowels[i]) {
+				result += consonants[consonants.length - 1];
+				break;
+			}
+			result += consonants[i];
+			result += vowels[i];
+		}
+	} else {
+		for (let i = 0; i < vowels.length; i++) {
+			if (!consonants[i]) {
+				result += vowels[vowels.length - 1];
+				break;
+			}
+			result += vowels[i];
+			result += consonants[i];
+		}
+	}
+	if (result.length !== s.length) {
+		return "failed";
+	} else {
+		return result;
+	}
+}
+
 // test cases
 solve("java"); // 'ajav'
 solve("oruder"); // 'edorur'
