74fafa9967091388f5fcaf9299083fb7c81503a9
Justin Hong <jhong2009@gmail.com>
2018-04-14 17:09:06 -0700

    in 7Kyu_AlternateCapitalization.js: added solution using a for loop

--- JavaScript Kata/7Kyu_AlternateCapitalization.js
+++ JavaScript Kata/7Kyu_AlternateCapitalization.js
@@ -4,6 +4,30 @@
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF'].
 // The input will be a lowercase string with no spaces.
 
+// input: a lowercase string with 0 spaces
+// iterate over the string
+// at an even idx, capitalize a char and send it to result[0]
+// at an odd idx, capitalize a char and send it to result[1]
+// output: an array with 2 elements
+// first el has chars at even indices capitalized
+// second el has chars at odd indices capitalized
+
+function capitalize(s) {
+	let result = ["", ""];
+
+	for (let i = 0; i < s.length; i++) {
+		if (i % 2 === 0) {
+			result[0] += s[i].toUpperCase();
+			result[1] += s[i];
+		} else {
+			result[0] += s[i];
+			result[1] += s[i].toUpperCase(0);
+		}
+	}
+	console.log("s", s);
+	return result;
+}
+
 // test cases
 capitalize("abcdef"); //['AbCdEf', 'aBcDeF']
 capitalize("codewars"); //['CoDeWaRs', 'cOdEwArS']
