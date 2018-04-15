74a9765565fd718da0ebb8b1a8176e1b18c13e3b
Justin Hong <jhong2009@gmail.com>
2018-04-14 17:08:17 -0700

    in 7Kyu_AlternateCapitalization.js: set up problem description and test cases

---
+++ JavaScript Kata/7Kyu_AlternateCapitalization.js
@@ -0,0 +1,13 @@
+// Description:
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately,
+// and return as shown below. Index 0 will be considered even.
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF'].
+// The input will be a lowercase string with no spaces.
+
+// test cases
+capitalize("abcdef"); //['AbCdEf', 'aBcDeF']
+capitalize("codewars"); //['CoDeWaRs', 'cOdEwArS']
+capitalize("abracadabra"); //['AbRaCaDaBrA', 'aBrAcAdAbRa']
+capitalize("codewarriors"); //['CoDeWaRrIoRs', 'cOdEwArRiOrS']
+capitalize("indexinglessons"); //['InDeXiNgLeSsOnS', 'iNdExInGlEsSoNs']
+capitalize("codingisafunactivity"); //['CoDiNgIsAfUnAcTiViTy', 'cOdInGiSaFuNaCtIvItY']
