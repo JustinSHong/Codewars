35ff38d15353b0f7692c8e6924c5e8dcac71ac66
Justin Hong <jhong2009@gmail.com>
2018-06-24 21:34:15 -0700

    in 8Kyu_AlternatingCase.c: added solution using strcpy() and for loop

--- C Kata/8Kyu_AlternatingCase.c
+++ C Kata/8Kyu_AlternatingCase.c
@@ -1,10 +1,43 @@
-//  Description
+//  Description:
+// Define String.prototype.toAlternatingCase 
+// (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; 
+// see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
 
-Define String.prototype.toAlternatingCase 
-(or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; 
-see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. 
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
-As usual, your function/method should be pure, i.e. it should not mutate the original string.
+// input: a string pointer
+// loop over each char in the input string
+  // char is lower - convert to upper
+  // char is upper - convert to lower
+// output: string with alternate casing
+
+char *to_alternating_case(const char *s) {
+  printf("input string: %s\n", s);
+  printf("input size: %d\n", strlen(s));
+  
+  int size;
+  int i;
+  size = strlen(s);
+  
+  char temp[size];
+  temp[size] = '\0';
+  strcpy(temp, s);
+  
+  for (i = 0; i < size; i++) {
+    if (islower(s[i])) {
+      // current char is lower case letter
+      temp[i] = toupper(s[i]);
+    } else { 
+      // current char is upper case letter
+      temp[i] = tolower(s[i]);
+    }
+    printf("temp: %c\n", temp[i]);
+  }
+  char *result[size];
+  strcpy(result, temp);
+  printf("result: %s\n", result);
+  return temp;
+}
 
 //Test Cases:
 to_alternating_case("hello world"); // => "HELLO WORLD"
