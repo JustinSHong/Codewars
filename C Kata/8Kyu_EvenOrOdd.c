dc37513a99c97708ba07367363b6be8f52eea55c
Justin Hong <jhong2009@gmail.com>
2018-06-29 17:20:42 -0700

    in 8Kyu_EvenOrOdd.c: add solution using if/else

--- C Kata/8Kyu_EvenOrOdd.c
+++ C Kata/8Kyu_EvenOrOdd.c
@@ -1,6 +1,15 @@
 // Description: 
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
+const char * even_or_odd(int number) {
+  // even if number / 2 is 0
+  if (number % 2 == 0) {
+    return "Even";
+  }
+  // odd if number / 2 is 1
+  return "Odd";
+};
+
 // Test Cases
 even_or_odd(90); // "Even"	
 even_or_odd(-80); // "Even"
