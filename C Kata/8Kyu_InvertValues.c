839516f91264b3947d2c550c6de7d61370b0f6e3
Justin Hong <jhong2009@gmail.com>
2018-06-29 17:33:08 -0700

    in 8Kyu_InvertValues.c: added kata description and test cases

---
+++ C Kata/8Kyu_InvertValues.c
@@ -0,0 +1,9 @@
+// Description:
+// Given a set of numbers, return the additive inverse of each. 
+// Each positive becomes negatives, and the negatives become positives.
+
+
+// Test Cases
+invert([1,2,3,4,5]); // [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]); // [-1,2,-3,4,-5]
+invert([]); // []
