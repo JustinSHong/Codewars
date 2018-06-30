8bd0b5cf9ec52719486233540ed31e6011d1db29
Justin Hong <jhong2009@gmail.com>
2018-06-29 17:19:42 -0700

    in 8Kyu_EvenOrOdd.c: added kata description and test cases

---
+++ C Kata/8Kyu_EvenOrOdd.c
@@ -0,0 +1,15 @@
+// Description: 
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// Test Cases
+even_or_odd(90); // "Even"	
+even_or_odd(-80); // "Even"
+even_or_odd(113); // "Odd"
+even_or_odd(9); // "Odd"
+even_or_odd(102); // "Even"
+even_or_odd(-94); // "Even"
+even_or_odd(101); // "Odd"
+even_or_odd(-101); // "Odd"
+even_or_odd(16); // "Even"
+even_or_odd(-27); // "Odd"
+even_or_odd(13); // "Odd"
