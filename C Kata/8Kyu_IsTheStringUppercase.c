1b8b06a9c2f5586f07e89546a8869ef792da314d
Justin Hong <jhong2009@gmail.com>
2018-07-06 15:10:55 -0700

    in 8Kyu_IsTheStringUppercase.c: added solution using for loop and isupper

--- C Kata/8Kyu_IsTheStringUppercase.c
+++ C Kata/8Kyu_IsTheStringUppercase.c
@@ -5,6 +5,37 @@
 // an ALL CAPS (uppercase) string will simply be defined as one containing no lowercase letters. 
 // Therefore, according to this definition, strings with no alphabetical characters should return True.
 
+// input: a string that may or may not have uppercase letters
+// initiate a pointer to point to source
+// loop over source
+// output: boolean
+  // true - input has an uppercase char
+  // false - input does not have any uppercase chars
+
+bool is_uppercase(const char *source) {
+  char *ptr;
+  int sourceLen;
+  
+  sourceLen = strlen(source);
+  ptr = source;
+  
+  // printf("source is %s\n", source);
+  // printf("source length is %d\n", sourceLen);
+  
+  for(int i = 0; i < sourceLen; i++) {
+    // printf("char %c\n",*ptr);
+
+    if (ptr < 64 && *ptr != 32) { // ignore corner cases
+      return false;
+    }
+    
+    if (!isupper(*ptr) && *ptr != ' ') {
+      return false;
+    }
+    ++ptr;
+  }
+  return true;
+};
 
 // Test Cases
 // is_uppercase("c");
