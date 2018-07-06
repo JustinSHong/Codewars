2911a66c9fc5c52adf5a3d22b6c837ff3761457c
Justin Hong <jhong2009@gmail.com>
2018-07-06 15:09:24 -0700

    in 8Kyu_IsTheStringUppercase.c: added kata description and test cases

---
+++ C Kata/8Kyu_IsTheStringUppercase.c
@@ -0,0 +1,15 @@
+// Description
+// Create a method is_uppercase() to see whether the string is ALL CAPS.
+// For simplicity, you will not be tested on the ability to handle corner cases 
+// (e.g. "%*&#()%&^#" or similar strings containing alphabetical characters at all) 
+// an ALL CAPS (uppercase) string will simply be defined as one containing no lowercase letters. 
+// Therefore, according to this definition, strings with no alphabetical characters should return True.
+
+
+// Test Cases
+// is_uppercase("c");
+// is_uppercase("C"); 
+// is_uppercase("hello I AM DONALD");
+// is_uppercase("HELLO I AM DONALD"); 
+// is_uppercase("ACSKLDFJSgSKLDFJSKLDFJ");
+// is_uppercase("ACSKLDFJSGSKLDFJSKLDFJ"); 
